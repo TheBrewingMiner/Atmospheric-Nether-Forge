ac4c126d0c16f3d9312c9eb42ae4d659946db365
aryanvirdi <asv2005@icloud.com>
2025-05-25 20:08:50 -0700

    Aquifer system now works when lava is the default liquid!

---
+++ src/main/java/net/thebrewingminer/atmosphericnether/mixin/AquiferMixin.java
@@ -0,0 +1,27 @@
+package net.thebrewingminer.atmosphericnether.mixin;
+
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.levelgen.Aquifer;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+@Mixin(Aquifer.NoiseBasedAquifer.class)
+public class AquiferMixin {
+    @Redirect(
+            method = "computeSubstance(Lnet/minecraft/world/level/levelgen/DensityFunction$FunctionContext;D)Lnet/minecraft/world/level/block/state/BlockState;",
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lnet/minecraft/world/level/block/state/BlockState;is(Lnet/minecraft/world/level/block/Block;)Z",
+                    ordinal = 0
+            )
+    )
+    private boolean reEnableAquiferOnLava(BlockState blockState, Block block){
+        if (block == Blocks.LAVA){
+            return false;       // Always skip check if default fluid is lava.
+        }
+        return blockState.is(block);  // If logic falls through, act as normal.
+    }
+}
