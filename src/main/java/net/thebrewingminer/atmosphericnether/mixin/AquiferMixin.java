03abbc055eb034d87a657b50c2b8704fb1feeb42
aryanvirdi <asv2005@icloud.com>
2025-05-27 17:33:45 -0700

    Fixed aquifer mixin logic to better emulate vanilla's behavior.

--- src/main/java/net/thebrewingminer/atmosphericnether/mixin/AquiferMixin.java
+++ src/main/java/net/thebrewingminer/atmosphericnether/mixin/AquiferMixin.java
@@ -4,6 +4,7 @@ import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.levelgen.Aquifer;
+import net.minecraft.world.level.levelgen.DensityFunction;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
@@ -18,10 +19,24 @@ public class AquiferMixin {
                     ordinal = 0
             )
     )
-    private boolean reEnableAquiferOnLava(BlockState blockState, Block block){
-        if (block == Blocks.LAVA){
-            return false;       // Always skip check if default fluid is lava.
-        }
-        return blockState.is(block);  // If logic falls through, act as normal.
+    private boolean reEnableAquiferOnLava(BlockState blockState, Block block, DensityFunction.FunctionContext context) {
+        int yLevel = context.blockY();
+        final int vanillaAquiferDisabledBelow = -54;        // Vanilla always fills eligible blocks with lava below y = -54.
+
+//        computeSubstance() decision flow:
+//        if (p_208187_ > 0.0) {
+//            this.shouldScheduleFluidUpdate = false;
+//            return null;
+//        } else {
+//            Aquifer.FluidStatus $$5 = this.globalFluidPicker.computeFluid($$2, $$3, $$4);
+//            if ($$5.at($$3).is(Blocks.LAVA)) {                <------ This if-statement is what is being mixed into.
+//                this.shouldScheduleFluidUpdate = false;               If true, aquifers are effectively "disabled" for lava.
+//                return Blocks.LAVA.defaultBlockState();               If false, continue to aquifer calculations.
+//            } else {...continue to aquifer calculations }             Therefore, we must trick the logic for lava to work.
+//    }
+
+        if (block == Blocks.LAVA && yLevel >= vanillaAquiferDisabledBelow){
+            return false;                               // If the block is lava and is above Vanilla's threshold for lava fill, "skip" the lava check, effectively re-enabling aquifer logic.
+        } else return blockState.is(block);             // Otherwise, check the fluid as usual.
     }
 }
