77d2ce5a323f820c6c2c9466b0011ebafe3e2140
aryanvirdi <asv2005@icloud.com>
2025-05-28 20:24:27 -0700

    Removed redeclarations of final fields, fixing the refusal to compile.

--- src/main/java/net/thebrewingminer/atmosphericnether/custom/feature/CustomizableLargeDripstoneConfiguration.java
+++ src/main/java/net/thebrewingminer/atmosphericnether/custom/feature/CustomizableLargeDripstoneConfiguration.java
@@ -15,30 +15,20 @@ public class CustomizableLargeDripstoneConfiguration extends LargeDripstoneConfi
             instance.group(
                     BlockStateProvider.CODEC.fieldOf("block").forGetter(cfg -> cfg.block),
                     TagKey.codec(Registry.BLOCK_REGISTRY).fieldOf("base_tag").forGetter(cfg -> cfg.baseTag),
-                    IntProvider.CODEC.fieldOf("column_radius").forGetter(cfg -> cfg.columnRadius),
-                    FloatProvider.CODEC.fieldOf("stalactite_bluntness").forGetter(cfg -> cfg.stalactiteBluntness),
-                    FloatProvider.CODEC.fieldOf("stalagmite_bluntness").forGetter(cfg -> cfg.stalagmiteBluntness),
-                    FloatProvider.CODEC.fieldOf("height_scale").forGetter(cfg -> cfg.heightScale),
-                    FloatProvider.CODEC.fieldOf("wind_speed").forGetter(cfg -> cfg.windSpeed),
-                    Codec.intRange(1, 128).fieldOf("min_radius_for_wind").forGetter(cfg -> cfg.minRadiusForWind),
+                    IntProvider.codec(1, 60).fieldOf("column_radius").forGetter(cfg -> cfg.columnRadius),
+                    FloatProvider.codec(0.1F, 10.0F).fieldOf("stalactite_bluntness").forGetter(cfg -> cfg.stalactiteBluntness),
+                    FloatProvider.codec(0.1F, 10.0F).fieldOf("stalagmite_bluntness").forGetter(cfg -> cfg.stalagmiteBluntness),
+                    FloatProvider.codec(0F, 20.0F).fieldOf("height_scale").forGetter(cfg -> cfg.heightScale),
+                    FloatProvider.codec(0.0F, 2.0F).fieldOf("wind_speed").forGetter(cfg -> cfg.windSpeed),
+                    Codec.intRange(1, 100).fieldOf("min_radius_for_wind").forGetter(cfg -> cfg.minRadiusForWind),
                     Codec.floatRange(0.0F, 5.0F).fieldOf("min_bluntness_for_wind").forGetter(cfg -> cfg.minBluntnessForWind),
-                    Codec.intRange(1, 512).fieldOf("floor_to_ceiling_search_range").forGetter(cfg -> cfg.floorToCeilingSearchRange),
+                    Codec.intRange(1, 512).fieldOf("floor_to_ceiling_search_range").orElse(30).forGetter(cfg -> cfg.floorToCeilingSearchRange),
                     Codec.floatRange(0.0F, 5.0F).fieldOf("max_column_radius_to_cave_height_ratio").forGetter(cfg -> cfg.maxColumnRadiusToCaveHeightRatio)
             ).apply(instance, CustomizableLargeDripstoneConfiguration::new)
     );
 
     public final BlockStateProvider block;
     public final TagKey<Block> baseTag;
-
-    public final IntProvider columnRadius;
-    public final FloatProvider stalactiteBluntness;
-    public final FloatProvider stalagmiteBluntness;
-    public final FloatProvider heightScale;
-    public final FloatProvider windSpeed;
-    public final int minRadiusForWind;
-    public final float minBluntnessForWind;
-    public final int floorToCeilingSearchRange;
-    public final float maxColumnRadiusToCaveHeightRatio;
 
     public CustomizableLargeDripstoneConfiguration(BlockStateProvider block, TagKey<Block> baseTag, IntProvider columnRadius, FloatProvider stalactiteBluntness, FloatProvider stalagmiteBluntness, FloatProvider heightScale, FloatProvider windSpeed, int minRadiusForWind, float minBluntnessForWind, int floorToCeilingSearchRange, float maxColumnRadiusToCaveHeightRatio) {
         super(floorToCeilingSearchRange, columnRadius, heightScale, maxColumnRadiusToCaveHeightRatio, stalactiteBluntness, stalagmiteBluntness, windSpeed, minRadiusForWind, minBluntnessForWind);
